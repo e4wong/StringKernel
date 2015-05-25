b0537375e6863078c362004de6ca2375793240b9
bgenchel <benjiegenchel@gmail.com>
2015-05-24 17:01:36 -0700

    just changed 'ds' to 'data_matrix'; just cloning and looking through what's here, reading problem description and stuff

--- StringKernel.py
+++ StringKernel.py
@@ -1,17 +1,19 @@
 import re
+import numpy
 
 def load(fn):
-	ds = []
+	data_matrix = []
 	f = open(fn, "r")
 	for line in f:
 		tokens = line.split()
 		data = (tokens[0], int(tokens[-1]))
-		ds.append(data)
-	return ds
+		data_matrix.append(data)
+	return data_matrix
 
 
 def main():
 	training_set = load("hw5train.txt")
 	test_set = load("hw5test.txt")
+
 if __name__ == '__main__':
 	main()
